5bc70fd0f68c993f304692381ab9560804555381
ADRDevp <magabriel1604@gmail.com>
2025-02-08 18:55:37 -0400

    Basic NavBar navigation IMP

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,8 +8,19 @@ import { HiOutlineHeart } from "react-icons/hi";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 
 import avatarImg from '../assets/avatar.png';
+import { useState } from 'react';
 
+
+const navigation = [
+  {name: 'Dashboard', href:"/dashboard"},
+  {name: 'Orders', href:"/orders"},
+  {name: 'Cart Page', href:"/cart"},
+  {name: 'Check Out', href:"/checkout"},
+]
 const Navbar = () => {
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  console.log(isDropdownOpen);
 
   const currentUser = true;
   return (
@@ -35,14 +46,35 @@ const Navbar = () => {
         {/* Right Side */}
         
         <div className='relative flex items-center md:space-x-3 space-x-2'>
-          <div>
+          <div className=''>
             {
               currentUser ? 
               <>
-                <button>
-                  <img src={avatarImg} alt='' className={`</>size-7 rounded full 
+                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  <img src={avatarImg} alt='' className={`</>size-7 rounded-full 
                   ${currentUser ? 'ring-2 ring-blue-500' : ''}`}/>
                 </button>
+
+              {/* Show Dropdowns */}
+              {
+                  isDropdownOpen &&
+                    <div className='absolute right-0 mt-2 w-48 bg-white
+                    shadow-lg rounded-md z-40'>
+                      <ul className='py-2'>
+                        {
+                          navigation.map((item) => (
+                            <li key={item.name} onClick={() => 
+                            setIsDropdownOpen(false)}>
+                                <Link to={item.href} className='block
+                                px-4 py-2 text-sm hover:bg-gray-100'>
+                                  {item.name}
+                              </Link>
+                            </li>
+                          ))
+                        }
+                      </ul>
+                    </div>
+              }
               </> : 
               <Link to="/login">
                 <HiOutlineUser className='size-6'/>
